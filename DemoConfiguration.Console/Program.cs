7b94e04471289609e8f7c88b29f6824b831b13b1
Lautaro Carro <lcarro@algeiba.com>
2023-03-06 12:12:04 -0300

    Add project files.

---
+++ DemoConfiguration.Console/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+using DemoConfiguration.Console;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+
+
+var envioroment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((_, configuration) =>
+    {
+        configuration.Sources.Clear();
+
+        configuration
+        .SetBasePath(Directory.GetCurrentDirectory())
+
+        .AddJsonFile("appsettings.json", false)
+
+        .AddJsonFile($"appsettings.{envioroment}.json", true)
+
+        .AddEnvironmentVariables();
+
+
+        string urlKeyVault = "";
+
+
+        if (!string.IsNullOrWhiteSpace(urlKeyVault))
+        {
+            var secretClient = new SecretClient(new(urlKeyVault), new DefaultAzureCredential());
+            configuration.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+        }
+
+
+        
+
+
+
+    })
+    .ConfigureServices((builder, services) =>
+    {
+
+        var connectionString = builder.Configuration.GetConnectionString("Default");
+
+
+        services.AddTransient<IUserService, UserService>();
+
+    })
+    .Build();
+
+
+var userService = host.Services.GetRequiredService<IUserService>();
+
+
+var defaultPassword = userService.GetDefaultPassword();
+
+
+Console.WriteLine("Hello World!");
+
---
+++ DemoConfiguration.Console/UserService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConfiguration.Console
+{
+    internal interface IUserService
+    {
+        string GetDefaultPassword();
+    }
+
+
+    internal class UserService : IUserService
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public string GetDefaultPassword()
+        {
+            return _configuration["Users:DefaultPassword"]!;
+
+        }
+    }
+}
